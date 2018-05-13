2fc6856bd2e8e4a0bc225e14fe17e0d14b8ff56a
Andrei Anisimov <1471231+andrei-anisimov@users.noreply.github.com>
2018-05-13 09:44:30 -0400

    meta mask

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,11 @@
 const Web3 = require('web3');
-const web3 = new Web3();
+let web3;
+if (typeof window.web3 !== 'undefined') {
+  console.log("Use MetaMask");
+  web3 = window.web3;
+} else {
+  web3 = new Web3();
+}  
 const contract = require('truffle-contract');
 
 // Import our contract artifacts and turn them into usable abstractions.
