1520f3fe921d55f5da58f5c690933424d3e9c5c6
Evgeniy Semushin <evgeniy.semushin@ecmasoft.com>
2018-05-13 09:07:51 -0400

    in progress

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import Profiles from './components/profiles';
 import TradeFieldAndGraphs from './components/trade/tradeFieldAndGraphs';
 import Form from './components/form';
 import './App.css';
+import { contractAPI } from "./api/index";
+
+const API_URL = "http://localhost:7545";
 
 class App extends Component {
   constructor(props) {
@@ -12,48 +15,63 @@ class App extends Component {
     this.state = {
       isPopUp: false
     };
+    this.web3API = null;
     this.handleButton = this.handleButton.bind(this);
+    console.log("test1", contractAPI);
+    contractAPI(API_URL).then((web3API) => {
+      this.web3API = web3API;
+    }).catch((e) => {
+      console.error("Error:", e.message);
+    });
+
   };
-  
+
   handleButton(event) {
     console.log(event.target);
     this.setState({
       isPopUp: true
     })
-};
+  };
 
-handleClose(event) {
-  console.log(event.target);
-  this.setState({
-    isPopUp: false
-  });
-};
-  
+  handleClose(event) {
+    console.log(event.target);
+    this.setState({
+      isPopUp: false
+    });
+  };
+
+  testGetTokenPrice = async () => {
+    console.log("here");
+    const price = await this.web3API.getTokenPrice();
+    console.log("price = ", price);
+  };
+
   render() {
     if (this.state.isPopUp) {
-    return (
-      <main className="sigmaTrade">
-         <div className="container">
-            <Form 
+      return (
+        <main className="sigmaTrade">
+          <div className="container">
+            <Form
               handleClose={this.handleClose}
             />
             <Header />
             <Profiles />
             <TradeFieldAndGraphs />
           </div>
-      </main>
-    )
-  } else {
-    return (
-      <main className="sigmaTrade">
-         <div className="container">
+        </main>
+      )
+    } else {
+      return (
+        <main className="sigmaTrade">
+          <div className="container">
+            <button onClick={this.testGetTokenPrice} >Test Clss 1</button>
             <Header />
-            <Profiles 
+            <Profiles
               handleButton={this.handleButton}
             />
             <TradeFieldAndGraphs />
           </div>
-      </main>
+        </main>
       );
     };
   };
